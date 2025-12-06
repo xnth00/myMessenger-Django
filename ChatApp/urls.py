3454f8ffa523a3e2472e01376e5bc236efd7c8e6
xnth00 <xosamper8511val@student.fatima.edu.ph>
2025-12-06 09:17:08 +0800

    Update urls.py

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns =[
+    path('', include('ChatApp.urls')
     path('', views.CreateRoom, name='create-room'),
     path('<str:room_name>/<str:username>/', views.MessageView, name='room'),
 ]
